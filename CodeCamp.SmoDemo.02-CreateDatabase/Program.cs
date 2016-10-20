27b53cfb501e7da9e67ca74bbf73b333bb7504a3
Thomas J. Cappelletti <tcappelletti@webstormtechnologies.com>
2016-10-20 06:59:32 -0400

    Added creating a database demo, still needs more items

---
+++ CodeCamp.SmoDemo.02-CreateDatabase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace CodeCamp.SmoDemo._02_CreateDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("localhost");
+
+            //TODO: Add files, extended properites, etc. to show a more complex example
+            Database database = new Database(server, "SmoDemoDatabase");
+            database.Create();
+        }
+    }
+}
