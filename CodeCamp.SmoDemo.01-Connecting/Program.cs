7157f93ecd19de09269095ae41851d2efbbcc404
Thomas J. Cappelletti <tcappelletti@webstormtechnologies.com>
2016-10-19 23:06:30 -0400

    Added project to demo connecting to a SQL Server instance

---
+++ CodeCamp.SmoDemo.01-Connecting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace CodeCamp.SmoDemo._01_Connecting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("localhost");
+
+            Console.WriteLine("Server Name: {0}", server.Name);
+            Console.WriteLine("Server Version: {0}", server.Version);
+            Console.WriteLine("Server Service Pack: {0}", server.ProductLevel);
+            Console.WriteLine("Server Edition: {0}", server.Edition);
+            Console.WriteLine("Server Collation: {0}", server.Collation);
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
