d07ee41da26ed87f1918e4e3a8dee58d7080358d
Thomas J. Cappelletti <tcappelletti@webstormtechnologies.com>
2016-10-20 21:24:38 -0400

    Made create database more complex

--- CodeCamp.SmoDemo.02-CreateDatabase/Program.cs
+++ CodeCamp.SmoDemo.02-CreateDatabase/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,48 @@ namespace CodeCamp.SmoDemo._02_CreateDatabase
         static void Main(string[] args)
         {
             Server server = new Server("localhost");
+            var primaryDataFileName = Path.Combine(server.DefaultFile, "SmoDemoDatabase_Primary.mdf");
+            var secondaryDataFileName = Path.Combine(server.DefaultFile, "SmoDemoDatabase_Secondary.ndf");
+            var logFileName = Path.Combine(server.DefaultLog, "SmoDemoDatabase_Log.ldf");
 
-            //TODO: Add files, extended properites, etc. to show a more complex example
             Database database = new Database(server, "SmoDemoDatabase");
+
+            FileGroup primaryFileGroup = new FileGroup(database, "PRIMARY", FileGroupType.RowsFileGroup);
+
+            database.FileGroups.Add(primaryFileGroup);
+
+            DataFile primaryDataFile = new DataFile(primaryFileGroup, "PrimaryDataFile")
+            {
+                FileName = primaryDataFileName,
+                Growth = 4096,
+                GrowthType = FileGrowthType.KB,
+                IsPrimaryFile = true,
+                Size = 10240
+            };
+
+            DataFile secondaryDataFile = new DataFile(primaryFileGroup, "SecondaryDataFile")
+            {
+                FileName = secondaryDataFileName,
+                Growth = 4096,
+                GrowthType = FileGrowthType.KB,
+                Name = "SecondaryDataFile",
+                Size = 10240
+            };
+
+            LogFile logFile = new LogFile(database, "LogFile", logFileName)
+            {
+                Growth = 2048,
+                GrowthType = FileGrowthType.KB,
+                Size = 20480
+            };
+
+            database.LogFiles.Add(logFile);
+
+            primaryFileGroup.Files.Add(primaryDataFile);
+            primaryFileGroup.Files.Add(secondaryDataFile);
+
+            server.Databases.Add(database);
+
             database.Create();
         }
     }
