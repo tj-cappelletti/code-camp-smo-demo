7bcc99fbf62f66b847751b41b62353685c5a71dc
Thomas J. Cappelletti <tcappelletti@webstormtechnologies.com>
2016-10-22 12:52:07 -0400

    Implemented running processes view

---
+++ CodeCamp.SmoDemo.07-RunningProcesses/RunningProcessesForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace CodeCamp.SmoDemo._07_RunningProcesses
+{
+    partial class RunningProcessesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runningProcessesDataGridView = new System.Windows.Forms.DataGridView();
+            this.refreshButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.runningProcessesDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // runningProcessesDataGridView
+            // 
+            this.runningProcessesDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.runningProcessesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.runningProcessesDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.runningProcessesDataGridView.Name = "runningProcessesDataGridView";
+            this.runningProcessesDataGridView.Size = new System.Drawing.Size(760, 509);
+            this.runningProcessesDataGridView.TabIndex = 0;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.refreshButton.Location = new System.Drawing.Point(697, 527);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // RunningProcessesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.runningProcessesDataGridView);
+            this.Name = "RunningProcessesForm";
+            this.Text = "Running Processes";
+            ((System.ComponentModel.ISupportInitialize)(this.runningProcessesDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView runningProcessesDataGridView;
+        private System.Windows.Forms.Button refreshButton;
+    }
+}
+
